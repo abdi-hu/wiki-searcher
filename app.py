fe3b9b0fc68155807c5f24248e6d0334e62b2710
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-06-07 19:59:31 -0400

    updated solution

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint
 from flask.json import jsonify
-from werkzeug.utils import redirect
+
 import requests
+import json
 
 app = Flask(__name__, subdomain_matching = True)
 
@@ -12,26 +13,22 @@ app.config["SERVER_NAME"] = "wiki-search.com:5000"
 
 @app.route('/', subdomain='<search_term>', methods=['GET'])
 def wiki(search_term):
-    uri = f"https://en.wikipedia.org/w/api.php?action=opensearch&search={search_term}&format=json"
-    req = requests.get(uri)
-    data = req.content
-    print('data')
-    print(data)
-    # return data
-   
-
-
-
-
-    return redirect(f"https://en.wikipedia.org/w/api.php?action=opensearch&search={search_term}&format=json")
     
+    uri = f"https://en.wikipedia.org/w/api.php?action=opensearch&search={search_term}"
+    req = requests.get(uri)
 
+    search_list = json.loads(req.content)
+    link_list = search_list[3]
 
+    return jsonify(lists=link_list)
+    
+    # Code below displays the first results if there's an exact match
+    # term = search_term.capitalize()
+    # if link_list[0]==f"https://en.wikipedia.org/wiki/{term}":
+    #     return jsonify(link_list[0])
+    # else:
+    #     return jsonify(lists=link_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-# curl command to test subdomain
-# curl --resolve 'dogs.wiki-search.com:5000' 127.0.0.1:5000
